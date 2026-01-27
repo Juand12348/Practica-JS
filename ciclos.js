780ef0ae34739dc925544505f1d1411062965730
Juan Martinez <juandavidmn0819@gmail.com>
2026-01-27 13:58:28 -0500

    Agregando archivo ciclos

---
+++ ciclos.js
@@ -0,0 +1,23 @@
+// Ciclo While
+
+console.log("*** Ciclo while ***");
+let numero = 0;
+
+while(numero < 10){
+    numero++;
+    console.log(numero);
+}
+
+console.log("*** Ciclo Do While ***");
+numero = 0;
+
+do{
+    numero++;
+    console.log(numero);
+}while(numero < 10);
+
+// Ciclo For
+console.log("*** Ciclo For ***");
+for(let n = 1; n < 11; n++){
+    console.log(n);
+}
