7c0c3be9ff456fcfe8624dab18ef6bd8bcafcc11
Juan Martinez <juandavidmn0819@gmail.com>
2026-01-27 15:18:22 -0500

    Termiando ejercicio nivel 1

--- ejercicios/ejercicio1.js
+++ ejercicios/ejercicio1.js
@@ -4,6 +4,31 @@ const a = 7;
 const b = 3;
 
 console.log("Suma: " + (a + b));
-console.log("Suma: " + (a - b));
-console.log("Suma: " + (a * b));
-console.log("Suma: " + (a / b));
+console.log("Resta: " + (a - b));
+console.log("Multiplicación: " + (a * b));
+console.log("División: " + (a / b));
+
+// Ejercicio 2: Condicional Básico
+console.log("*** Ejercicio Condicional Básico ***");
+const numero = 7;
+if(numero > 0){
+    console.log("Positivo");
+}else if(numero < 0){
+    console.log("Negativo");
+}else{
+    console.log("Cero");
+}
+
+// Ejercicio 3: Ciclo Simple
+console.log("*** Ejercicio Ciclo Simple ***");
+for(let i = 1; i < 11; i++){
+    console.log(i);
+}
+
+// Ejercicio 4: Función básico
+console.log("*** Ejercicio Función básica ***")
+function saludar(nombre){
+    console.log(`Hola, ${nombre}`)
+}
+
+saludar("Juancho");
