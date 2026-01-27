eaa37dd107703adec86ed5bb4f0799c5b68af9ae
Juan Martinez <juandavidmn0819@gmail.com>
2026-01-27 14:03:39 -0500

    Agregando archivo arreglos

---
+++ arreglos.js
@@ -0,0 +1,17 @@
+// Arreglos
+console.log("*** Arreglos ***");
+
+// Primera forma
+let array = new Array('BMW', 'Mercedez', 'Volvo');
+
+// Senguda forma
+const autos = ["Porshe", "Audi", "Ferrari"];
+
+// Agregar valores
+autos.push("Lamborhini");
+
+// Conocer el tamaño del arreglo
+console.log(autos.length);
+
+// Conocer si una variable es una arreglo
+console.log(Array.isArray(autos));
