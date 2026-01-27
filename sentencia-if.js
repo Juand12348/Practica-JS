dc711ab0f2420735135d0578fa46208e0b5c37ac
Juan Martinez <juandavidmn0819@gmail.com>
2026-01-27 13:41:17 -0500

    Agregando archivo sentencia-if

---
+++ sentencia-if.js
@@ -0,0 +1,11 @@
+// Sentacia IF
+console.log("*** Sentencia IF ***");
+
+const numero1 = 9;
+const numero2 = 7;
+
+if (numero1 > numero2){
+    console.log("El número 1 es mayor");
+}else{
+    console.log("El número 2 es mayor");
+}
